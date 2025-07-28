6f9f44ca5c1b20d46ae61295fba1c4fef22907bf
Farhad Rezazadeh, PhD <47423984+frezazadeh@users.noreply.github.com>
2025-07-28 21:08:05 +0100

    Create model.py

---
+++ model.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNNModel(nn.Module):
+    """Simple CNN with BatchNorm and Dropout for MNIST."""
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.bn1   = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn2   = nn.BatchNorm2d(64)
+        self.pool  = nn.MaxPool2d(2)
+        self.drop  = nn.Dropout(0.25)
+        self.fc1   = nn.Linear(64 * 7 * 7, 128)
+        self.fc2   = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.drop(x)
+        return F.log_softmax(self.fc2(x), dim=1)
